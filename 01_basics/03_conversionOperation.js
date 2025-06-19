4af9ac3e56534063c6b4d1591f6864f05cd0daba
Akanksha2996 <mishraakanksha9629@gmail.com>
2025-06-19 13:34:27 +0000

    Conversion and Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,41 @@ let someString = 55
 let othervalue = String(someString)
 console.log(othervalue);
 console.log(typeof othervalue);
+
+
+// ********************** operations **********************
+
+
+let value = 3
+let nagValue = -value
+console.log(nagValue);
+
+// console.log(2+2);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(5-5);
+
+let str1 = "Hello"
+let str2 = " Akanksha"
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "3");
+// console.log("2" + 3 + 3);
+// console.log(3 + 3 + "5");
+// console.log(4 + "2" + 6);
+// console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// prefix a++ and post fix ++b
+
+
+
+
+
