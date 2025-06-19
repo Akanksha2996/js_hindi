7ff38ab485b776819fd5b4824bd4920c9d72dc6d
Akanksha2996 <mishraakanksha9629@gmail.com>
2025-06-19 12:02:05 +0000

    container config done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "akanksha@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ahmedabad"
+let accountState;
+
+
+accountEmail ="abc@gmailcom"
+accountPassword = "987"
+accountCity ="jaipur"
+
+// accountId = 2
+console.log(accountId)
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
