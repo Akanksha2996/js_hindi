f1845be9643a78ac9a1021d3a60b9109c9c565cc
Akanksha2996 <mishraakanksha9629@gmail.com>
2025-06-19 12:08:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+ "use strict"; //treat all js code as newer vewrsion
+//  alert(3+3)  WE R using node js, not browser
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// Boolean => true/false
+// null => standard alone Value
+// undefined =>
+// symbol =>uniqness 
+
+
+// objects
+console.log(typeof null)
+console.log(typeof 23)
+console.log(typeof "null")
+console.log(typeof undefined)
