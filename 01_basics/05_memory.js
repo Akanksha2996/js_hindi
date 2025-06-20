d2e482375cdd98691f3cb5bff8e3ca201039e323
Akanksha2996 <mishraakanksha9629@gmail.com>
2025-06-20 13:15:03 +0530

    Update 05_memory.js

--- 01_basics/05_memory.js
+++ 01_basics/05_memory.js
@@ -1,1 +1,24 @@
+// memory allocations
+// stack memory(primitive), and heap (Non-PRIMITIVE)
+//  stack memory(primitive)  - JO VAR DECLARE KIUYA H USKA DUPLICATE MILTA H
+//  heap (Non-PRIMITIVE) - Jo var declare kiya uski original value  milti h
 
+// stack memory(primitive)
+
+let akankshaMail = "abc@123gmail.com"
+let othermail = akankshaMail
+othermail = "akashay@123gmail.com"
+console.log(othermail);
+console.log(akankshaMail);
+
+
+// Heap  memory(non-primitive)
+
+let myUser1 = {
+  email: "hrabc@gmail.com",
+  upi: "user@124e3"
+}
+let myUser2 = myUser1
+myUser2.email = "aaku@gmail.com"
+console.log(myUser2);
+console.log(myUser1);
